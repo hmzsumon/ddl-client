c46dc7a9062b0bdcf43513934b3f1437632325ee
hmzsumon <zsumonn@gmail.com>
2025-02-14 21:41:21 +0600

    first commit

--- components/layout/HeroSection.tsx
+++ components/layout/HeroSection.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const HeroSection = () => {
 	return (
-		<div className=' mt-14 '>
+		<div className=' mt-10 '>
 			<h1 className='text-5xl text-center md:text-left md:text-6xl font-bold'>
 				Join the global <br className=' hidden md:block' /> live-streaming
 				platform
@@ -11,10 +11,13 @@ const HeroSection = () => {
 			<p className='text-center md:text-2xl font-bold mt-5'>
 				for content creation, social communication, and live entertainment.
 			</p>
-			<div className='flex justify-center md:justify-start '>
-				<button className='flex btn-gradient mt-5 py-3  rounded-2xl  items-center justify-center gap-3 text-xl font-bold w-full'>
+			<div className=' '>
+				<button className='flex btn-gradient mt-5 py-3  rounded-3xl  items-center justify-center gap-3 text-xl font-bold w-full'>
 					<CircleUser size={25} />
 					Log in / Sign up
+				</button>
+				<button className='flex bg-white text-gray-800 mt-2 py-3  rounded-3xl  items-center justify-center gap-3 text-xl font-bold w-full'>
+					Get DDL App
 				</button>
 			</div>
 		</div>
